837e54e24328b52c0e5bd92c5d282dd27b0b4f58
astonixplays <114138464+astonixplays@users.noreply.github.com>
2026-03-24 17:27:21 +0600

    Add files via upload

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,10 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import { createClient } from '@supabase/supabase-js';
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  }
-);
+
+// Initialize database client
+const supabaseUrl = 'https://eezpbcbgnogbvmdasduy.databasepad.com';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImQ3NTEzNDU4LWEyMDEtNDY1Ni04YjUzLWU1YTVlNDU0NzdhNyJ9.eyJwcm9qZWN0SWQiOiJlZXpwYmNiZ25vZ2J2bWRhc2R1eSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzcwMDU2MjY1LCJleHAiOjIwODU0MTYyNjUsImlzcyI6ImZhbW91cy5kYXRhYmFzZXBhZCIsImF1ZCI6ImZhbW91cy5jbGllbnRzIn0.uFqfEOND2DlapZIlRqGuOS40FLZ2SGp7_YnvNxm225o';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+export { supabase };
